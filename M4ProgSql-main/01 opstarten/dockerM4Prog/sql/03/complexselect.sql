82757df2b4d2f96e160206e76d1601871bb78758
Isaiah2am <isaiahkoroma@hotmail.com>
2025-05-15 12:08:10 +0200

    03

--- M4ProgSql-main/01 opstarten/dockerM4Prog/sql/03/complexselect.sql
+++ M4ProgSql-main/01 opstarten/dockerM4Prog/sql/03/complexselect.sql
@@ -1,5 +1,5 @@
-SELECT * FROM `huisdieren` order BY typedier desc;
-SELECT * FROM `huisdieren` LIMIT 2;
-SELECT SUM(leeftijd) FROM `huisdieren`;
+SELECT * FROM `mydata` order BY name desc;
+SELECT * FROM `mydata` LIMIT 2;
+SELECT SUM(tijdgebruikt) FROM `mydata`;
 
-SELECT SUM(leeftijd) FROM `huisdieren` order BY typedier desc LIMIT 2;
+SELECT SUM(tijdgebruikt) FROM `mydata` order BY tijdgebruikt desc LIMIT 2;
